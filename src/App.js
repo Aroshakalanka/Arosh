7ef9ec3876d14a3a2acc0db31ceb57cd61bb9ba7
Arosh Akalanka <daakalankap@gmail.com>
2022-06-05 17:17:47 +0530

    zoho mail 3.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Navigate, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import HomeScreen from "./screens/home/HomeScreen";
